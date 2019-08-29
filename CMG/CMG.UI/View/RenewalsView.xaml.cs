161ec9abf00b0029e077310b926af0b349058328
kavita.patel19 <kavita.patel19@ymail.com>
2019-08-29 16:20:45 -0400

    added renewals view

---
+++ CMG/CMG.UI/View/RenewalsView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CMG.UI.View
+{
+    /// <summary>
+    /// Interaction logic for RenewalsView.xaml
+    /// </summary>
+    public partial class RenewalsView : Window
+    {
+        public RenewalsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
