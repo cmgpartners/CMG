2f9448e2ec0135598975228abff2472005c2bf5d
Rishita <Rishita@CMG-DT28>
2019-09-05 16:49:08 -0400

    Review changes for dbScript

--- CMG/CMG.DataAccess/Scripts/0001_AgentTable_Create.sql
+++ CMG/CMG.DataAccess/Scripts/0001_AgentTable_Create.sql
@@ -5,16 +5,12 @@ BEGIN
 		Id int NOT NULL IDENTITY(1,1),
 		FirstName varchar(50) NOT NULL,
 		LastName varchar(50) NOT NULL,
-		--CommissionId int NOT NULL,
-		--Commission decimal,
-		--Split decimal,
 		AgentCode varchar(5) NOT NULL,
 		IsDeleted bit default(0) NOT NULL,
 		CreatedDate datetime NOT NULL,
 		CreatedBy varchar(50) NOT NULL,
 		ModifiedDate datetime,
 		ModifiedBy varchar(50),
-		CONSTRAINT PK_Agent PRIMARY KEY (Id),
-		--FOREIGN KEY (CommissionId) REFERENCES [dbo].[COMM](KEYCOMM) AgentCommission
+		CONSTRAINT PK_Agent PRIMARY KEY (Id)
    )
 END
