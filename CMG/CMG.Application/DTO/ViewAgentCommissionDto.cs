d522d731188c1f4e94e90171875d80babb1145d3
Rishita <Rishita@CMG-DT28>
2019-09-19 14:33:45 -0400

    Code clean up and added Insured column in Commission table (temporary column Insured in commission table)

--- CMG/CMG.Application/DTO/ViewAgentCommissionDto.cs
+++ CMG/CMG.Application/DTO/ViewAgentCommissionDto.cs
@@ -4,13 +4,8 @@ namespace CMG.Application.DTO
 {
     public class ViewAgentCommissionDto
     {
-        public ViewAgentCommissionDto()
-        {
-            Agent = new ViewAgentDto();
-        }
-        public string FirstName { get; set; }
         public decimal Commission { get; set; }
         public decimal Split { get; set; }
-        public ViewAgentDto Agent { get; set; }
+        public ViewAgentDto Agent { get; set; } = new ViewAgentDto();
     }
 }
--- CMG/CMG.Application/Mapper/CommissionMapperProfile.cs
+++ CMG/CMG.Application/Mapper/CommissionMapperProfile.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using AutoMapper;
+﻿using AutoMapper;
 using CMG.Application.DTO;
-using CMG.Common;
 using CMG.DataAccess.Domain;
 
 namespace CMG.Application.Mapper
@@ -10,109 +8,19 @@ namespace CMG.Application.Mapper
     {
         public CommissionMapperProfile()
         {
-            CreateMap<Comm, ViewCommissionDto>()
-                .ForMember(des => des.PolicyNumber, mo => mo.MapFrom(src => src.Policynum))
-                .ForMember(des => des.CompanyName, mo => mo.MapFrom(src => src.Company))
-                .ForMember(des => des.InsuredName, mo => mo.MapFrom(src => src.Insured))
-                .ForMember(des => des.Renewal, mo => mo.MapFrom(src => src.Renewals))
-                .ForMember(des => des.TotalAmount, mo => mo.MapFrom(src => src.Total))
-                .ForMember(des => des.Agents, mo => mo.MapFrom(src => new List<ViewAgentCommissionDto>()))
-                .AfterMap(AfterMapAgents);
-
             CreateMap<Commission, ViewCommissionDto>()
                 .ForMember(des => des.CommissionId, mo => mo.MapFrom(src => src.Id))
                 .ForMember(des => des.PolicyNumber, mo => mo.MapFrom(src => src.Policy.Policynum))
                 .ForMember(des => des.CompanyName, mo => mo.MapFrom(src => src.Policy.Company))
-                .ForMember(des => des.InsuredName, mo => mo.MapFrom(src => "InsuredName-Test"))
+                .ForMember(des => des.InsuredName, mo => mo.MapFrom(src => src.Insured))
                 .ForMember(des => des.Renewal, mo => mo.MapFrom(src => src.RenewalType))
                 .ForMember(des => des.TotalAmount, mo => mo.MapFrom(src => src.Total))
-                .ForMember(des => des.Agents, mo => mo.MapFrom(src => new List<ViewAgentCommissionDto>()))
-                .AfterMap(AfterMapAgentCommissions);
+                .ForMember(des => des.AgentCommissions, mo => mo.MapFrom(src => src.AgentCommission))
+                ;
 
-            CreateMap<Agent, ViewAgentDto>();
-        }
-
-        public void AfterMapAgentCommissions(Commission source, ViewCommissionDto des)
-        {
-            ViewAgentCommissionDto agentCommission;
-            List<AgentCommission> agentCommissions = new List<AgentCommission>(source.AgentCommission);
-            if (source != null)
-            {
-                for (int i = 0; i < source.AgentCommission.Count; i++)
-                {
-                    agentCommission = new ViewAgentCommissionDto();
-                    agentCommission.FirstName = agentCommissions[i].Agent.FirstName;
-                    agentCommission.Commission = agentCommissions[i].Commission.HasValue ? (decimal)agentCommissions[i].Commission.Value : 0;
-                    agentCommission.Split = agentCommissions[i].Split.HasValue ? (decimal)agentCommissions[i].Split.Value : 0;
-
-                    agentCommission.Agent.Id = agentCommissions[i].Agent.Id;
-                    agentCommission.Agent.FirstName = agentCommissions[i].Agent.FirstName;
-                    agentCommission.Agent.LastName = agentCommissions[i].Agent.LastName;
-                    agentCommission.Agent.MiddleName = agentCommissions[i].Agent.MiddleName;
-                    agentCommission.Agent.AgentCode = agentCommissions[i].Agent.AgentCode;
-                    agentCommission.Agent.Color = agentCommissions[i].Agent.Color;
-                    agentCommission.Agent.IsDeleted = agentCommissions[i].Agent.IsDeleted;
-                    des.Agents.Add(agentCommission);
-                }
-            }
-        }
-
-            public void AfterMapAgents(Comm source, ViewCommissionDto des)
-        {
-            var agents = new List<ViewAgentCommissionDto>();
-            var agent = new ViewAgentCommissionDto();
-            if (source != null)
-            {
-                if (source.Marty > 0)
-                {
-                    agent.FirstName = Enums.AgentEnum.Kate.ToString();
-                    agent.Commission = source.Marty;
-                    agent.Split = source.Split1;
-                    agents.Add(agent);
-                }
-
-                if (source.Peter > 0)
-                {
-                    agent = new ViewAgentCommissionDto();
-                    agent.FirstName = Enums.AgentEnum.Peter.ToString();
-                    agent.Commission = source.Peter;
-                    agent.Split = source.Split2;
-                    agents.Add(agent);
-                }
-                if (source.Frank > 0)
-                {
-                    agent = new ViewAgentCommissionDto();
-                    agent.FirstName = Enums.AgentEnum.Frank.ToString();
-                    agent.Commission = source.Frank;
-                    agent.Split = source.Split3;
-                    agents.Add(agent);
-                }
-                if (source.Bob > 0)
-                {
-                    agent = new ViewAgentCommissionDto();
-                    agent.FirstName = Enums.AgentEnum.Bob.ToString();
-                    agent.Commission = source.Bob;
-                    agent.Split = source.Split4;
-                    agents.Add(agent);
-                }
-                if (source.Mary > 0)
-                {
-                    agent = new ViewAgentCommissionDto();
-                    agent.FirstName = Enums.AgentEnum.Mary.ToString();
-                    agent.Commission = source.Mary;
-                    agent.Split = source.Split5;
-                    agents.Add(agent);
-                }
-                if (source.Other > 0)
-                {
-                    agent = new ViewAgentCommissionDto();
-                    agent.FirstName = Enums.AgentEnum.Others.ToString();
-                    agent.Commission = source.Other;
-                    agent.Split = source.Split6;
-                    agents.Add(agent);
-                }
-            }
-            des.Agents = agents;
+            CreateMap<AgentCommission, ViewAgentCommissionDto>()
+                .ForMember(des => des.Commission, mo => mo.MapFrom(src => src.Commission.HasValue ? (decimal)src.Commission.Value : 0))
+                .ForMember(des => des.Split, mo => mo.MapFrom(src => src.Split.HasValue ? (decimal)src.Split.Value : 0));
         }
     }
 }
