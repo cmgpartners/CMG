1293c6f0a34dcc68cdec35e7c9f5827b9f0b16ad
kavita.patel19 <kavita.patel19@ymail.com>
2019-10-22 09:38:44 -0400

    addedb FYC screen to populate FYC data

---
+++ CMG/CMG.Application/ViewModel/FYCViewModel.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using CMG.Application.DTO;
+using CMG.DataAccess.Interface;
+using CMG.DataAccess.Query;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace CMG.Application.ViewModel
+{
+    public class FYCViewModel : MainViewModel
+    {
+        #region Member variables
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private const int startYear = 1925;
+        private int newId;
+        #endregion Member variables
+
+        #region Constructor
+        public FYCViewModel(IUnitOfWork unitOfWork, IMapper mapper)
+            : base(unitOfWork, mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            LoadData();
+        }
+        #endregion Constructor
+        #region Properties
+        public ICollection<string> Months
+        {
+            get
+            {
+                return DateTimeFormatInfo.CurrentInfo.MonthNames.Where(t => t.Length > 0).Select(m => m.Substring(0, 3)).ToList();
+            }
+        }
+
+        public ICollection<int> Years
+        {
+            get
+            {
+                return Enumerable.Range(startYear, DateTime.Now.Year + 1 - startYear).ToList();
+            }
+        }
+        private int _selectedYear = DateTime.Now.Year;
+        public int SelectedYear
+        {
+            get { return _selectedYear; }
+            set
+            {
+                _selectedYear = value;
+                OnPropertyChanged("SelectedYear");
+                GetCommissions();
+            }
+        }
+
+        private string _selectedMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).ToString("MMM", CultureInfo.InvariantCulture);
+        public string SelectedMonth
+        {
+            get { return _selectedMonth; }
+            set
+            {
+                _selectedMonth = value;
+                OnPropertyChanged("SelectedMonth");
+                GetCommissions();
+            }
+        }
+        private ObservableCollection<ViewCommissionDto> _dataCollection;
+        public ObservableCollection<ViewCommissionDto> DataCollection
+        {
+            get { return _dataCollection; }
+            set
+            {
+                _dataCollection = value;
+                OnPropertyChanged("DataCollection");
+                OnPropertyChanged("IsNoRecordFound");
+            }
+        }
+        #endregion
+        #region Methods
+
+        public void GetCommissions()
+        {
+            SearchQuery searchQuery = BuildFYCSearchQuery(SelectedYear, SelectedMonth);
+            var dataSearchBy = _unitOfWork.Commissions.Find(searchQuery);
+            DataCollection = new ObservableCollection<ViewCommissionDto>(dataSearchBy.Result.Select(r => _mapper.Map<ViewCommissionDto>(r)).ToList());
+        }
+        private void LoadData()
+        {
+            GetCommissions();
+        }
+
+        private SearchQuery BuildFYCSearchQuery(int year, string month)
+        {
+            SearchQuery searchQuery = new SearchQuery();
+            List<FilterBy> filterBy = new List<FilterBy>();
+            var fromPayDate = new DateTime(year, Array.IndexOf(Months.ToArray(), month) + 1, 1);
+            var toPayDate = fromPayDate.AddMonths(1).AddDays(-1);
+            filterBy.Add(FilterByRange("PayDate", fromPayDate.ToShortDateString(), toPayDate.ToShortDateString()));
+            filterBy.Add(FilterByEqual("FYC", "F"));
+            searchQuery.FilterBy = filterBy;
+            return searchQuery;
+        }
+
+        private FilterBy FilterByRange(string propertyName, string greaterThan, string lessThan)
+        {
+            FilterBy filterBy = new FilterBy();
+            filterBy.Property = propertyName;
+            filterBy.GreaterThan = greaterThan;
+            filterBy.LessThan = lessThan;
+            return filterBy;
+        }
+
+        private FilterBy FilterByEqual(string propertyName, string value)
+        {
+            FilterBy filterBy = new FilterBy();
+            filterBy.Property = propertyName;
+            filterBy.Equal = value;
+            return filterBy;
+        }
+        #endregion
+    }
+}
