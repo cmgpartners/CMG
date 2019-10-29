e3d1d4388bbd59cc71104b86d07ff134e3e4562f
kavita.patel19 <kavita.patel19@ymail.com>
2019-10-29 10:58:49 -0400

    implemented scaffold after AgentWithdrawal table

--- CMG/CMG.DataAccess/Domain/Withd.cs
+++ CMG/CMG.DataAccess/Domain/Withd.cs
@@ -1,25 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CMG.DataAccess.Domain
 {
-    public partial class Withd
+    public partial class Withd : EntityBase
     {
+        public Withd()
+        {
+            AgentWithdrawal = new HashSet<AgentWithdrawal>();
+        }
+        [Key]
         public int Keywith { get; set; }
         public string Dtype { get; set; }
         public string Yrmo { get; set; }
         public string Desc { get; set; }
         public string Ctype { get; set; }
-        public decimal Marty { get; set; }
-        public decimal Peter { get; set; }
-        public decimal Frank { get; set; }
-        public decimal Bob { get; set; }
-        public decimal Mary { get; set; }
-        public decimal Other { get; set; }
-        public string RevLocn { get; set; }
-        public DateTime RevDate { get; set; }
-        public string Cr8Locn { get; set; }
-        public DateTime Cr8Date { get; set; }
-        public bool Del { get; set; }
+        public virtual IEnumerable<AgentWithdrawal> AgentWithdrawal { get; set; }
     }
 }
