08c7514618a7a6ffebae6451a526c9dc936397b2
Rishita <Rishita@CMG-DT28>
2019-10-30 09:28:12 -0400

    Finance Summary screen with raw data in ViewModel

--- CMG/CMG.Application/ViewModel/FinanceSummaryViewModel.cs
+++ CMG/CMG.Application/ViewModel/FinanceSummaryViewModel.cs
@@ -2,9 +2,9 @@
 using CMG.DataAccess.Interface;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 
 namespace CMG.Application.ViewModel
 {
@@ -23,6 +23,7 @@ namespace CMG.Application.ViewModel
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            GetUsers();            
         }
         #endregion Constructor
 
@@ -54,11 +55,44 @@ namespace CMG.Application.ViewModel
                 //GetCommissions();
             }
         }
+
+        private ObservableCollection<User> _dataCollection;
+        public ObservableCollection<User> DataCollection
+        {
+            get { return _dataCollection; }
+            set
+            {
+                _dataCollection = value;
+                OnPropertyChanged("DataCollection");
+                OnPropertyChanged("IsNoRecordFound");
+            }
+        }
         #endregion Properties
 
 
         #region Methods
+        public void GetUsers()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Max Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+            users.Add(new User() { Id = 4, Name = "Next Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            DataCollection = new ObservableCollection<User>();
+            foreach (var user in users)
+                DataCollection.Add(user);
+        }
 
         #endregion Methods
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
     }
 }
