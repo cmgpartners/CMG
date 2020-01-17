e83fd3c398de7f8a9da5192ad6507bb70dea5044
Rishita <Rishita@CMG-DT28>
2020-01-17 13:52:23 -0500

    Bug fixing PolicyMain ViewModel and MainViewModel related issues

--- CMG/CMG.Application/ViewModel/MainViewModel.cs
+++ CMG/CMG.Application/ViewModel/MainViewModel.cs
@@ -164,7 +164,15 @@ namespace CMG.Application.ViewModel
             {
                 _policyCollection = value;
                 OnPropertyChanged("PolicyCollection");
+                OnPropertyChanged("IsNoRecordFound");
             }
+        }
+        public bool IsNoRecordFound
+        {
+            get
+            {
+                return PolicyCollection != null && PolicyCollection.Count == 0;
+            }            
         }
         private ViewClientSearchDto _selectedClient;
         public ViewClientSearchDto SelectedClient
@@ -217,7 +225,7 @@ namespace CMG.Application.ViewModel
                 _policies = value;
                 OnPropertyChanged("Policies");
             }
-        }
+        }        
         private List<string> _entityTypes;
         public List<string> EntityTypes
         {
