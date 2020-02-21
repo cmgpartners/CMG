0cc5d46cb96c715ab958ab81e07a33cd90fe2602
kavita.patel19 <kavita.patel19@ymail.com>
2020-02-21 11:06:32 -0500

    completed add/edit/delete functionalities for company and status on configuration screen

--- CMG/CMG.Application/ViewModel/ConfigurationViewModel.cs
+++ CMG/CMG.Application/ViewModel/ConfigurationViewModel.cs
@@ -70,7 +70,10 @@ namespace CMG.Application.ViewModel
         {
             get { return CreateCommand(SaveCompany); }
         }
-
+        public ICommand CancelCompanyCommand
+        {
+            get { return CreateCommand(CancelCompany); }
+        }
         public ICommand AddStatusCommand
         {
             get { return CreateCommand(AddStatus); }
@@ -82,6 +85,10 @@ namespace CMG.Application.ViewModel
         public ICommand SaveStatusCommand
         {
             get { return CreateCommand(SaveStatus); }
+        }
+        public ICommand CancelStatusCommand
+        {
+            get { return CreateCommand(CancelStatus); }
         }
         #endregion
         private new void LoadData()
@@ -104,7 +111,7 @@ namespace CMG.Application.ViewModel
         }
         public void AddCompany()
         {
-            Companies.Add(new ViewComboDto() { Id  = --newId });
+            Companies.Add(new ViewComboDto() { Id = --newId });
         }
         public void AddStatus()
         {
@@ -120,12 +127,134 @@ namespace CMG.Application.ViewModel
         }
         private void SaveCompany()
         {
-            SaveComboItem(Companies, OriginalCompanies);
-            GetCompanies();
+            try
+            {
+                foreach (ViewComboDto comboItem in Companies)
+                {
+                    if (string.IsNullOrEmpty(comboItem.Description))
+                    {
+                        _notifier.ShowError("Please enter company name");
+                        return;
+                    }
+                    var originalComboItem = OriginalCompanies?.Where(item => item.Id == comboItem.Id).FirstOrDefault();
+                    if (comboItem.Id > 0)
+                    {
+                        var comboItemJson = JsonConvert.SerializeObject(comboItem);
+                        var originalComboItemJson = JsonConvert.SerializeObject(originalComboItem);
+                        if (originalComboItem == null || !(comboItemJson == originalComboItemJson))
+                        {
+                            var entity = _mapper.Map<Combo>(comboItem);
+                            entity.Rev_Date = DateTime.Now;
+                            entity.Rev_Locn = $"{Environment.UserDomainName}\\{Environment.UserName}";
+                            _unitOfWork.Combo.Save(entity);
+                        }
+                    }
+                    else
+                    {
+                        if (OriginalCompanies.Any(c => c.Description.Trim().ToLower() == comboItem.Description.Trim().ToLower()))
+                        {
+                            _notifier.ShowError("Company already exist");
+                            return;
+                        }
+                        var entity = _mapper.Map<Combo>(comboItem);
+                        string[] splits = entity.DESC_.Split(" ");
+                        string code = string.Empty;
+                        Random random = new Random();
+                        if (splits.Length > 1)
+                        {
+                            code = $"{splits[0][0]}{splits[1][0]}";
+                        }
+                        else
+                        {
+                            code = $"{splits[0][0]}{splits[0][splits[0].Length - 1]}";
+                        }
+                        while (OriginalCompanies.Any(c => c.FieldCode.ToLower() == code.ToLower()))
+                        {
+                            code = RandomString(2, random);
+                        }
+                        entity.Id = 0;
+                        entity.FLDCODE = code.ToUpper();
+                        entity.FIELDNAME = CompanyFieldName;
+                        entity.Rev_Date = DateTime.Now;
+                        entity.Rev_Locn = $"{Environment.UserDomainName}\\{Environment.UserName}";
+                        _unitOfWork.Combo.Add(entity);
+                    }
+                }
+                _unitOfWork.Commit();
+                GetCompanies();
+                _notifier.ShowSuccess("Record added/updated successfully");
+            }
+            catch
+            {
+                _notifier.ShowError("Error occured while add/update record");
+            }
         }
         private void SaveStatus()
         {
-            SaveComboItem(PolicyStatus, OriginalPolicyStatus, StatusFieldName);
+            try
+            {
+                foreach (ViewComboDto comboItem in PolicyStatus)
+                {
+                    if (string.IsNullOrEmpty(comboItem.Description))
+                    {
+                        _notifier.ShowError("Please enter status");
+                        return;
+                    }
+                    var originalComboItem = OriginalPolicyStatus?.Where(item => item.Id == comboItem.Id).FirstOrDefault();
+                    if (comboItem.Id > 0)
+                    {
+                        var comboItemJson = JsonConvert.SerializeObject(comboItem);
+                        var originalComboItemJson = JsonConvert.SerializeObject(originalComboItem);
+                        if (originalComboItem == null || !(comboItemJson == originalComboItemJson))
+                        {
+                            var entity = _mapper.Map<Combo>(comboItem);
+                            entity.Rev_Date = DateTime.Now;
+                            entity.Rev_Locn = $"{Environment.UserDomainName}\\{Environment.UserName}";
+                            _unitOfWork.Combo.Save(entity);
+                        }
+                    }
+                    else
+                    {
+                        if (OriginalPolicyStatus.Any(c => c.Description.Trim().ToLower() == comboItem.Description.Trim().ToLower()))
+                        {
+                            _notifier.ShowError("Status already exist");
+                            return;
+                        }
+                        var entity = _mapper.Map<Combo>(comboItem);
+                        string[] splits = entity.DESC_.Split(" ");
+                        string code = string.Empty;
+                        Random random = new Random();
+                        if (splits.Length > 0)
+                        {
+                            code = $"{splits[0][0]}";
+                            while (OriginalPolicyStatus.Any(c => c.FieldCode.ToLower() == code.ToLower()))
+                            {
+                                code = RandomString(1, random);
+                            }
+                        }
+                        entity.Id = 0;
+                        entity.FLDCODE = code.ToUpper();
+                        entity.FIELDNAME = StatusFieldName;
+                        entity.Rev_Date = DateTime.Now;
+                        entity.Rev_Locn = $"{Environment.UserDomainName}\\{Environment.UserName}";
+                        _unitOfWork.Combo.Add(entity);
+                    }
+                }
+                _unitOfWork.Commit();
+                GetPolicyStatus();
+                _notifier.ShowSuccess("Record added/updated successfully");
+            }
+            catch
+            {
+                _notifier.ShowError("Error occured while add/update record");
+            }
+        }
+        private void CancelCompany()
+        {
+            GetCompanies();
+        }
+        private void CancelStatus()
+        {
             GetPolicyStatus();
         }
 
@@ -152,41 +281,11 @@ namespace CMG.Application.ViewModel
                 _notifier.ShowError("Error occured while deleting record");
             }
         }
-        private void SaveComboItem(ObservableCollection<ViewComboDto> collection, ObservableCollection<ViewComboDto> originalCollection, string comboItemType = CompanyFieldName)
+        public static string RandomString(int length, Random random)
         {
-            try
-            {
-                foreach (ViewComboDto comboItem in collection)
-                {
-                    var originalComboItem = originalCollection?.Where(item => item.Id == comboItem.Id).FirstOrDefault();
-                    if (comboItem.Id > 0)
-                    {
-                        var comboItemJson = JsonConvert.SerializeObject(comboItem);
-                        var originalComboItemJson = JsonConvert.SerializeObject(originalComboItem);
-                        if (originalComboItem == null || !(comboItemJson == originalComboItemJson))
-                        {
-                            var entity = _mapper.Map<Combo>(comboItem);
-                            _unitOfWork.Combo.Save(entity);
-                        }
-                    }
-                    else
-                    {
-                        var entity = _mapper.Map<Combo>(comboItem);
-                        string[] splits = entity.DESC_.Split(" ");
-                        if(splits.Length > 1)
-                        {
-
-                        }
-                        _unitOfWork.Combo.Add(entity);
-                    }
-                }
-                _unitOfWork.Commit();
-                _notifier.ShowSuccess("Record added/updated successfully");
-            }
-            catch
-            {
-                _notifier.ShowError("Error occured while add/update record");
-            }
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
         #endregion
     }
